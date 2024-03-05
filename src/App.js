50dba41ddba8be6cd79d1bc677e11be17f734ab1
unknown <zsimox07@gmail.com>
2024-03-05 09:12:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1>ciao resmini</h1>
+        </div>
+      </header>
+      <body>
+        <h2>Dio è con te</h2>
+      </body>
+    </div>
+  );
+}
+
+export default App;
